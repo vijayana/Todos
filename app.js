428cf43e090947658b4cf2ac7964c160107fd7cd
vijayana <30928700+vijayana@users.noreply.github.com>
2021-11-15 22:42:58 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const todo = require("./routes/todos");
+const connection = require("./db");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+connection();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/todos", todo);
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
